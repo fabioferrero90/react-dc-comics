976239e2f2cc61ada571e29474e250311c73917a
fabioferrero90 <fabio.ff90@gmail.com>
2025-01-13 13:31:57 +0100

    fix: Components import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer'; 
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   )
 }
 
